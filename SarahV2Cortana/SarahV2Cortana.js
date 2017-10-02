a3487eeef0edd8fb0c4b0dc6becaa9b75ac56a87
seb03000 <avatardu33@gmail.com>
2017-10-02 14:03:15 +0200

    Update SarahV2Cortana.js

--- SarahV2Cortana/SarahV2Cortana.js
+++ SarahV2Cortana/SarahV2Cortana.js
@@ -1,5 +1,5 @@
 exports.init = function(SARAH){
-SARAH.speak('initialisation terminé, Bonjour monsieur.');
+SARAH.speak('initialisation terminé, Bonjour monsieur, mon interface et prèt.');
  }
 
 //Réalisation de SarahV2Cortana par Avatar Rousseau (Dark Best), alias seb03000.
@@ -15,31 +15,27 @@ switch(data.val) {
 	Txt[0] = "Mode Pleine écran activé";
 	break;	
 		
-        case "runrainmeter": //lance rainmeter
+    case "runrainmeter": //lance rainmeter
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runrainmeter.bat';
 	var Txt = new Array; 
-	Txt[0] = "j'affiche l'interface";
-	Txt[1] = "je lance mon interface";
+	Txt[0] = "je lance mon interface";
 	break;
 
 	case "closerainmeter": //fermer rainmeter
 	var process = '%CD%/plugins/SarahV2Cortana/bin/closerainmeter.bat';
 	var Txt = new Array; 
-	Txt[0] = "c'est fait monsieur";
-	Txt[1] = "interface fermé";
+	Txt[0] = "interface fermé";
 	break;	
 	  
 	case "konsole": // affiche le terminale de windows 7
 	var process = '%CD%/plugins/SarahV2Cortana/bin/konsole.bat';
-	break;
+	Txt[0] = "terminal activé";
+	break; 
 	  
     case "controlpanel": // affiche le panneau de configuration 
 	var process = '%CD%/plugins/SarahV2Cortana/bin/controlpanel.bat';
+	Txt[0] = "Affichage du panneau de configuration";
 	break;  
-	  
-	case "f5": //actualise ou rafraichie les pages
-	var process = '%CD%/plugins/SarahV2Cortana/bin/f5.vbs'; 
-	break; 
 	  
 	case "runSteam": // ouvre le logiciel steam
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runSteam.bat';
@@ -99,8 +95,8 @@ switch(data.val) {
 	var process = '%CD%/plugins/SarahV2Cortana/bin/closeclavier.bat';
 	var Txt = new Array; 
 	Txt[0] = "Le clavier virtuéle vas se fermer";
-    break;
-		
+    break;  
+	  
 	case "runBluetooth": // ouvre le périphérique du bluetooth
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runBluetooth.bat';
 	var Txt = new Array; 
@@ -126,7 +122,9 @@ switch(data.val) {
     break; 
 	  
 	case "Fichier": // créer un fichier sur le bureau
-	var process = '%CD%/plugins/SarahV2Cortana/bin/Fichier.bat'; 
+	var process = '%CD%/plugins/SarahV2Cortana/bin/Fichier.bat';
+	var Txt = new Array; 
+	Txt[0] = "votre fichée a été crée sur votre bureau";
     break;
 	
 	case "scanne_avaste": // ouvre le logiciel antivirus avaste
@@ -137,48 +135,62 @@ switch(data.val) {
 	
 	case "Dossier": // créer un dossier sur le bureau
 	var process = '%CD%/plugins/SarahV2Cortana/bin/Dossier.bat'; 
+	var Txt = new Array; 
+	Txt[0] = "votre dossier a été crée sur votre burau";
     break;
 	
 	case "selectall": // pérmet de souligné 
 	var process = '%CD%/plugins/SarahV2Cortana/bin/selectall.vbs';
     var Txt = new Array; 
-	Txt[0] = "en coure de surlignement";
+	Txt[0] = "surlignement effectué";
 	break;
 	
 	case "cancel": // permet de quitté
 	var process = '%CD%/plugins/SarahV2Cortana/bin/cancel.vbs';
+	var Txt = new Array; 
+	Txt[0] = "je suprime";
 	break;
 	
 	case "retablir": // permet de reméttre 
 	var process = '%CD%/plugins/SarahV2Cortana/bin/retablir.vbs';
+	var Txt = new Array; 
+	Txt[0] = "je rétabli";
 	break;
 	
 	case "save": // permet de sauvegarder
 	var process = '%CD%/plugins/SarahV2Cortana/bin/save.vbs';
+	var Txt = new Array; 
+	Txt[0] = "fichier sauvegardé";
 	break;
 	
 	case "copy": // permet de faire une copie
 	var process = '%CD%/plugins/SarahV2Cortana/bin/copy.vbs';
+	var Txt = new Array; 
+	Txt[0] = "contenu copié";
 	break;
 	
 	case "paste": // permet de coller
 	var process = '%CD%/plugins/SarahV2Cortana/bin/paste.vbs';
+	var Txt = new Array; 
+	Txt[0] = "contenu collé";
 	break;
 	
 	case "cut": // permet de couper
 	var process = '%CD%/plugins/SarahV2Cortana/bin/cut.vbs';
+	var Txt = new Array; 
+	Txt[0] = "contenu coupé";
 	break;
 	  
 	case "enter": // appui touche entréer pour valider ou sortir l'écran qui se serait mus en veille
-	process = '%CD%/plugins/jarvis/bin/enter.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/enter.vbs';	
 	var Txt = new Array; 
-	Txt[0] = "je valide";	
-		
+	Txt[0] = "voilà monsieur, je";
+	break;
+	
 	case "journale": //permet d'aller sur un site voir le journale
 	var process = '%CD%/plugins/SarahV2Cortana/bin/journale.bat';
 	var Txt = new Array; 
-	Txt[0] = "voici les info monsieur";
-	Txt[1] = "affichage des info";
+	Txt[0] = "voici les dernières nouvelles du journal";
 	break; 
 	
 	case "process": //affichage du processeur en fonction
@@ -189,43 +201,50 @@ switch(data.val) {
 	
     case "bfm": // permet d'aller sur le site BFM télé
 	var process = '%CD%/plugins/SarahV2Cortana/bin/bfm.bat';
+	var Txt = new Array; 
+	Txt[0] = "connexion avec le direct de la chaine BFM Tévé";
 	break;
 	  
 	case "ebay": // Aller sur le site de EBAY
 	var process = '%CD%/plugins/SarahV2Cortana/bin/ebay.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mai ibé";
 	break;
 	
 	case "leboncoin": // Aller sur le site leboncoin
 	var process = '%CD%/plugins/SarahV2Cortana/bin/leboncoin.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mai leboncoin";
 	break;
 	  
     case "corbeille": // permet de vider votre corbeille de votre bureau
     var process = '%CD%/plugins/SarahV2Cortana/bin/corbeille.bat';
 	var Txt = new Array; 
 	Txt[0] = "votre corbeille et vidé.";
-	Txt[1] = "vidage de votre corbeille";
 	break;
 	
     case "allo": // permet d'aller sur le site alociné
 	var process = '%CD%/plugins/SarahV2Cortana/bin/allo.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vais vous trouver une recette sur internet";
 	break;
 	
 	case "lock": // permet de vérrouiller votre session
 	var process = '%CD%/plugins/SarahV2Cortana/bin/lock.bat';
+	var Txt = new Array; 
+	Txt[0] = "Verrouillage de votre session";
 	break;
 	
 	case "ToggleWindow":
     var process = '%CD%/plugins/SarahV2Cortana/ahk/ToggleWindow.ahk';
 	var Txt = new Array; 
 	Txt[0] = "Mode réduit activé";
-	Txt[1] = "c'est réduit, monsieur";
 	break;
 	  
   	case "runxbmc": // affiche le logiciel XBMC
  	var process = '%CD%/plugins/SarahV2Cortana/ahk/RunXBMC.ahk';
 	var Txt = new Array; 
 	Txt[0] = "ixbéhèmecé est en cours de lancement";
-	Txt[1] = "ixbéhèmecé vas se lancé";
 	break;
 	  
 	case "closexbmc": // ferme le logiel XBMC
@@ -238,7 +257,6 @@ switch(data.val) {
  	var process = '%CD%/plugins/SarahV2Cortana/bin/runkodi.bat';
 	var Txt = new Array; 
 	Txt[0] = "kodi est en cours de lancement";
-	Txt[1] = "kodi vas se lancé";
 	break;
 	  
 	case "closekodi": // ferme le logiel kodi
@@ -275,36 +293,36 @@ switch(data.val) {
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runfirefox.bat';
 	var Txt = new Array; 
 	Txt[0] = "j'ouvre firefox";
-	Txt[1] = "je lance firefox";
 	break;
 	  
 	case "closefirefox": //ferme firefox
 	var process = '%CD%/plugins/SarahV2Cortana/bin/Closefirefox.bat';
 	var Txt = new Array; 
 	Txt[0] = "fermeture de firefox";
-	Txt[1] = "firefox fermé";
 	break;
 		
 	case "rungooglechrome": //lance googlechrome
 	var process = '%CD%/plugins/SarahV2Cortana/bin/rungooglechrome.bat';
 	var Txt = new Array; 
 	Txt[0] = "j'ouvre gougueulechrome";
-	Txt[1] = "je lance gougueulechrome";
 	break;
 	
 	case "closegooglechrome": //ferme googlechrome
 	var process = '%CD%/plugins/SarahV2Cortana/bin/Closegooglechrome.bat';
 	var Txt = new Array; 
 	Txt[0] = "fermeture de gougueulechrome";
-	Txt[1] = "gougueulechrome fermé";
 	break;	
 	  
 	case "runshutdown": // arrêté le pc
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runshutdown.bat';
+	var Txt = new Array; 
+	Txt[0] = "Arré du pc en coure";
 	break;
 	  
 	case "runrestart": // redémarre votre pc
 	var process = '%CD%/plugins/SarahV2Cortana/bin/runrestart.bat';
+	var Txt = new Array; 
+	Txt[0] = "redémarage de votre pc";
 	break;
   
 	case "runvlc": // ouvre le logiciel VLC
@@ -358,48 +376,49 @@ switch(data.val) {
 	case "runnotepad": // ouvre le logiciel notpad
     var process = '%CD%/plugins/SarahV2Cortana/bin/runnotepad.bat';
 	var Txt = new Array; 
-	Txt[0] = "j'ouvre l'editeur";
-	Txt[1] = "je lance notpade";
+	Txt[0] = "je lance notpade";
 	break;
 	
 	case "closenotepad": // ferme le logiciel notpad
     var process = '%CD%/plugins/SarahV2Cortana/bin/closenotpad.bat';
 	var Txt = new Array; 
-	Txt[0] = "éditeur fermé monsieur";
-	Txt[1] = "notpad fermé";
+	Txt[0] = "notpad fermé";
 	break;
 	
 	case "runBlocnote": // ouvre le blocnote
     var process = '%CD%/plugins/SarahV2Cortana/bin/runBlocnote.bat';
 	var Txt = new Array; 
-	Txt[0] = "j'ouvre l'editeur";
-	Txt[1] = "je lance Blocnote";
+	Txt[0] = "je lance Blocnote";
 	break;
 	
 	case "closeBlocnote": // ferme le blocnote
     var process = '%CD%/plugins/SarahV2Cortana/bin/closeBlocnote.bat';
 	var Txt = new Array; 
-	Txt[0] = "éditeur fermé monsieur";
-	Txt[1] = "Blocnote fermé";
+	Txt[0] = "Blocnote fermé";
 	break;
 	
 	case "runreboot": // redémarre le client est serveur de sarah
 	var process = '%CD%/plugins/SarahV2Cortana/ahk/runreboot.ahk';
+	var Txt = new Array; 
+	Txt[0] = "je réinitialise mon programme";
 	break;
 	  
 	case "start_invisible": // Redémarage du serveur node + client micro en mode invisible( en arrière plan )
 	var process = '%CD%/plugins/SarahV2Cortana/bin/start_invisible.vbs';
+	var Txt = new Array; 
+	Txt[0] = "je redémarre mon système en arrière plan";
 	break;
 	  
 	case "éteindre_système": // Redémarage du serveur node + client micro en mode invisible( en arrière plan )
 	var process = '%CD%/plugins/SarahV2Cortana/bin/éteindre_système.vbs';
+	var Txt = new Array; 
+	Txt[0] = "jéteind mon sytème";
 	break; 	
 		
 	case "minimize": // reduit les page pour aller sur votre bureau
-	process = '%CD%/plugins/SarahV2Cortana/bin/minimize.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/minimize.vbs';
 	var Txt = new Array; 
-	Txt[0] = "voici le bureau";
-	Txt[1] = "affichage du bureau";
+	Txt[0] = "affichage du bureau";
 	break;	  
 	  
 	case "plugins": // ouvre le dossier plugins de sarah
@@ -410,190 +429,218 @@ switch(data.val) {
 	
 	case "dossier": // ouvre le dossier de sarah
 	var process = '%CD%/plugins/SarahV2Cortana/bin/sarah.bat';
+	var Txt = new Array; 
+	Txt[0] = "j'ouvre mon dossier";
 	break;
 	
 	case "images": // ouvre les image
-	var process = '%CD%/plugins/SarahV2Cortana/bin/images.bat'; 
+	var process = '%CD%/plugins/SarahV2Cortana/bin/images.bat';
+	var Txt = new Array; 
+	Txt[0] = "Voici vos image";
 	break;
 	
 	case "Films": // ouvre l'emplacement du dossier de film
 	var process = '%CD%/plugins/SarahV2Cortana/bin/Films.bat';
+	var Txt = new Array; 
+	Txt[0] = "Voici vos filme";
 	break;
 	
 	case "documents":
 	var process = '%CD%/plugins/SarahV2Cortana/bin/documents.bat';
+	var Txt = new Array; 
+	Txt[0] = "voici vos document";
 	break;
 
 	case "screenOff": //eteint l'écran
-	process = '%CD%/plugins/SarahV2Cortana/bin/screenoff.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/screenoff.bat';
+	var Txt = new Array; 
+	Txt[0] = "l'écran et en coure de fermeture";
 	break;
 	
 	case "screenOn": //allume l'écran
-	process = '%CD%/plugins/SarahV2Cortana/bin/screenon.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/screenon.bat';
+	var Txt = new Array; 
+	Txt[0] = "affichage de l'écran";
 	break;
 	  
 	case "runMatrix": // Mode veille Matrix
-	process = '%CD%/plugins/SarahV2Cortana/bin/runMatrix.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runMatrix.bat';
 	var Txt = new Array; 
 	Txt[0] = "Mode veille activé";
-	Txt[1] = "exécution en cour";
 	break; 
 	
 	case "tracabilitéIP": // Mode tracabilité IP
-	process = '%CD%/plugins/SarahV2Cortana/bin/tracabilitéIP.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/tracabilitéIP.vbs';
 	var Txt = new Array; 
 	Txt[0] = "Mode tracabilité lancé";
-	Txt[1] = "exécution en cour";
 	break;
 		
 	case "facebook": // ouvre le site facebook
-	process = '%CD%/plugins/SarahV2Cortana/bin/facebook.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/facebook.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà facebouke";
-	Txt[1] = "je vous lance tout de suis facebouke";
 	break;
 	
     case "forum": // ouvre mon forum
-	process = '%CD%/plugins/SarahV2Cortana/bin/forum.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/forum.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà votre forum";
-	Txt[1] = "je vous lance votre forum";
 	break;
 	   
 	case "youtube": // ouvre youtube
-	process = '%CD%/plugins/SarahV2Cortana/bin/youtube.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/youtube.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà youtube";
-	Txt[1] = "c'est fait youtube et ouvère";
 	break;
 	
     case "Gmail": // ouvre la page de gmail la messagerie
-	process = '%CD%/plugins/SarahV2Cortana/bin/Gmail.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/Gmail.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà monsieur, affichage de votre méssageri";
 	break;
 	
 	case "communauté": // ouvre la comunauté
-	process = '%CD%/plugins/SarahV2Cortana/bin/communauté.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/communauté.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà monsieur, affichage de la communauté";
 	break;
 	
 	case "Gougueule": //ouvre la page google
-	process = '%CD%/plugins/SarahV2Cortana/bin/Google.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/Google.bat';
 	var Txt = new Array; 
 	Txt[0] = "voilà Gougueule";
-	Txt[1] = "c'est fait Gougueule et ouvère";
 	break;
 	
 	case "Twitter": //ouvre twitter
-	process = '%CD%/plugins/SarahV2Cortana/bin/Twitter.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/Twitter.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mais tuiteur";
 	break;
 	
 	case "store": // ouvre le store
-	process = '%CD%/plugins/SarahV2Cortana/bin/store.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/store.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mais le store";
 	break;
 	
     case "pagejaune": // ouvre le site page jaune
-	process = '%CD%/plugins/SarahV2Cortana/bin/pagejaune.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/pagejaune.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mais les page jaune";
 	break;
 	
 	case "emule": // ouvre le site emule island 
-	process = '%CD%/plugins/SarahV2Cortana/bin/emule.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/emule.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mai émule";
 	break;
 	
 	case "Cdiscount": // ouvre le site  Cdiscount
-	process = '%CD%/plugins/SarahV2Cortana/bin/Cdiscount.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/Cdiscount.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mais Cdiscount";
 	break;
 	
 	case "wikipedia": // ouvre le site wikipedia
-	process = '%CD%/plugins/SarahV2Cortana/bin/wikipedia.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/wikipedia.bat';
+	var Txt = new Array; 
+	Txt[0] = "Je vous mais wikipédia";
 	break;
 	
 	case "restore": // restauration des fenêtre 
-	process = '%CD%/plugins/SarahV2Cortana/bin/restore.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/restore.vbs';
 	var Txt = new Array; 
-	Txt[0] = "affichage des fenêtres";
-	Txt[1] = "restoration des fenêtres en coure";
+	Txt[0] = "restoration des fenêtres en coure";
 	break;
 	
 	case "global": // lance la vue 3D aéro de windows 7
-	process = '%CD%/plugins/SarahV2Cortana/bin/global.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/global.bat';
 	var Txt = new Array; 
-	Txt[0] = "affichage de la vue aéro";
-	Txt[1] = "affichage des fenêtres en 3D";
+	Txt[0] = "affichage des fenêtres en 3D";
 	break;
 	
-        case "global": // lance la vue 3D aéro de windows 7 et Windows 10
-	process = '%CD%/plugins/SarahV2Cortana/bin/global.vbs';
-	var Txt = new Array; 
-	Txt[0] = "affichage de la vue aéro";
-	Txt[1] = "affichage des fenêtres en 3D";
-	break;	
-		
 	case "next": // suivant pour une page
-	process = '%CD%/plugins/SarahV2Cortana/bin/next.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/next.vbs';
 	var Txt = new Array; 
 	Txt[0] = "suivant";
 	break;
 	
 	case "precedent": // precedent pour une page
-	process = '%CD%/plugins/SarahV2Cortana/bin/precedent.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/precedent.vbs';
 	var Txt = new Array; 
-	Txt[0] = "retour en arrière";
-	Txt[1] = "précédent";
+	Txt[0] = "précédent";
 	break;
 	
 	case "save": // sauvegarder
-	process = '%CD%/plugins/SarahV2Cortana/bin/save.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/save.vbs';
 	var Txt = new Array; 
-	Txt[0] = "je sauvegarde";
-	Txt[1] = "sauvegarde éffectué";
+	Txt[0] = "sauvegarde éffectué";
 	break;
 	
 	case "shut":// fermé les fenêtre
-	process = '%CD%/plugins/SarahV2Cortana/bin/shut.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/shut.vbs';
 	var Txt = new Array; 
 	Txt[0] = "fermeture des fenêtre";
 	break;
 	
 	case "dvdon": // ouvre le lecteur CD de votre pc
-	process = '%CD%/plugins/SarahV2Cortana/bin/dvdon.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/dvdon.bat';
 	var Txt = new Array; 
 	Txt[0] = "lecteur ouvert";
 	break;
 	
 	case "dvdoff": // ferme le lecteur CD de votre pc
-	process = '%CD%/plugins/SarahV2Cortana/bin/dvdoff.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/dvdoff.bat';
 	var Txt = new Array; 
 	Txt[0] = "lecteur fermé monsieur";
 	break;
 	
 	case "runInfoPC": // Ouvre le fichier InfoPC.bat
-	process = '%CD%/plugins/SarahV2Cortana/bin/runInfoPC.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runInfoPC.bat';
 	var Txt = new Array; 
 	Txt[0] = "Vos information de votre pc et en cour d'ouverture";
 	break;	
 		
 	case "close": // fermer tous
-	process = '%CD%/plugins/SarahV2Cortana/bin/close.vbs';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/close.vbs';
 	break;
-		
-        case "runcapture-décran": // Lance une capture d'écran
-	process = '%CD%/plugins/SarahV2Cortana/bin/runcapture-décran.bat';
-	break;	
-		
-	case "runpetit-jeux": // Lance la démo du jeux
-	process = '%CD%/plugins/SarahV2Cortana/bin/runpetit-jeux.bat';
-	break;	
-		
+	
+	case "runcapture-décran": // Lance une capture décran
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runcapture-décran.bat';
+	var Txt = new Array; 
+	Txt[0] = "je vous fait une capture décran";
+	break;
+	
+    case "runpetit-jeux": // Lance la démo du jeux
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runpetit-jeux.bat';
+	var Txt = new Array; 
+	Txt[0] = "je vous lance le jeux";
+	break;
+	
 	case "runRadio": // Lance la Radio-Dark-Best
-	process = '%CD%/plugins/SarahV2Cortana/bin/runRadio.bat';
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runRadio.bat';
+	var Txt = new Array; 
+	Txt[0] = "je vous lance la Radio";
 	break;
-		
+	
 	case "runNavigateur": // Lance le Navigateur Web SarahV2Cortana
-	process = '%CD%/plugins/SarahV2Cortana/bin/runNavigateur.bat';
-	break;	
+	var process = '%CD%/plugins/SarahV2Cortana/bin/runNavigateur.bat';
+	var Txt = new Array; 
+	Txt[0] = "je vous Lance le Navigateur";
+	break;
+	
+	case "f5": //actualise ou rafraichie les pages
+	var process = '%CD%/plugins/SarahV2Cortana/bin/f5.vbs';
+	var Txt = new Array; 
+	Txt[0] = "Actualisation éffectuer";
+	break;
+	
+	//raccourcis clavier
+	case "f4": //Ctrl+F4 fermeture de l'onglet
+	var process = '%CD%/plugins/jarvis/bin/f4.vbs';
+	var Txt = new Array; 
+	Txt[0] = "onglet fermé";
+	break;
   }    
 
  var child = exec(process,
